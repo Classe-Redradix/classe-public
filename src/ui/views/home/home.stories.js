806ac1285531b05db3ffa17f9b97d45cf36899ac
DavidRedradix <david@redradix.com>
2021-05-31 16:36:39 +0200

    Simplify stories

--- src/ui/views/home/home.stories.js
+++ src/ui/views/home/home.stories.js
@@ -2,13 +2,11 @@ import React from 'react'
 import MainWrapper from '../../components/layout/MainWrapper'
 
 export default {
-  title: 'Views/Home'
+  title: 'Views'
 }
 
-export const template = () => (
+export const Home = () => (
   <MainWrapper>
     <p>Hello CLASS[E]</p>
   </MainWrapper>
 )
-
-template.storyName = 'Home'
