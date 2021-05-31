306085aae7ff193277debe099a484aab3f73856f
DavidRedradix <david@redradix.com>
2021-05-31 15:25:59 +0200

    Add mixins

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import { addDecorator } from '@storybook/react'
 import withIntl from './addons/intl/withIntl'
 import { registerIcon } from '../src/styleguide/styleguide-config'
-// import '!style-loader!css-loader!sass-loader!../src/assets/stylesheets/style.scss'
+import '!style-loader!css-loader!sass-loader!../src/assets/stylesheets/style.scss'
 
 const iconReq = require.context(
   '!raw-loader!../src/assets/icons/',
