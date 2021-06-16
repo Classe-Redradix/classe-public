44a569ebc36aec8d32cf121fa0ca0d643009acb7
DavidRedradix <david@redradix.com>
2021-06-16 13:28:21 +0200

    modify featured section

--- src/ui/components/sections/Featured.jsx
+++ src/ui/components/sections/Featured.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import useTranslations from '../../../hooks/useTranslations'
 import SectionWrapper from '../wrappers/SectionWrapper'
 import Row from '../row/Row'
 import Cell from '../cell/Cell'
-import useTranslations from '../../../hooks/useTranslations'
+import Button from '../button/Button'
 import demoImage from '../../../assets/images/demo.png'
 
 const Featured = () => {
@@ -13,15 +14,16 @@ const Featured = () => {
       <Row type="half">
         <Cell hasGap>
           <div className="scrambleTextWrapper">
-            <h3 className="h3 scrambleText">
-              {t('manifest:claim', {
+            <h3 className="h1 scrambleText">
+              {t('featured:title', {
                 line: (text) => <span className="line">{text}</span>,
                 lineTab: (text) => <span className="line has-tab">{text}</span>,
               })}
             </h3>
           </div>
+          <Button isLink isFluor isFull href="/" text={t('featured:button')} />
         </Cell>
-        <Cell hasGap>
+        <Cell>
           <img src={demoImage} alt="" />
         </Cell>
       </Row>
