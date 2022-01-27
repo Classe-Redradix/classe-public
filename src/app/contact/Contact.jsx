8b8bc55e89e028a77830f09777b3ee26a76eedc7
rr21pepe <pepe@redradix.com>
2022-01-27 11:31:40 +0100

    ✨ Detect course in contact page
    * Also verify if the course Id exists in the courses list

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -1,7 +1,28 @@
+import useQueryParams from '../../hooks/useQueryParams'
+import COURSES from './../../../pages/courses/courses'
+
 const Contact = () => {
+  const courseId = useQueryParams('course-id')
+
+  const selectedCourseExists = !!courseId
+    ? !!COURSES.find(course => course.id === courseId)
+    : null
+
   return (
-    <section>
+    <section
+      style={{
+        maxWidth: 500,
+        margin: '2rem auto',
+      }}
+    >
       <h1>Contact</h1>
+      {!!courseId ? (
+        selectedCourseExists ? (
+          <p>Selected course: {courseId}</p>
+        ) : (
+          <p>Selected course ({courseId}) doesn't exists</p>
+        )
+      ) : null}
     </section>
   )
 }
