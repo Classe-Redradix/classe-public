b4d189d7f32a1a57ab1bcfbc14a827ae4a7ebbcd
rr21pepe <pepe@redradix.com>
2022-01-27 09:15:21 +0100

    🤡 Add courses data

---
+++ pages/courses/courses.js
@@ -0,0 +1,244 @@
+const COURSE_JS_PRO = {
+  id: 'js-pro',
+  information: {
+    name: 'JS PRO',
+    nextCallDate: '2022-06-01',
+    description: "This is the best JavaScript course you'll ever find",
+    price: 900,
+    hours: 20,
+    places: 30,
+    enrolledStudents: 13,
+    successPercentage: 95,
+  },
+  index: [
+    {
+      number: '01',
+      name: 'Fundamentos',
+      description:
+        'Introducción y recursos. String templates. Desestructuración. Declaración de variables. (2H)',
+    },
+    {
+      number: '02',
+      name: 'Programación funcional',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones. (2H)',
+    },
+    {
+      number: '03',
+      name: 'Programación orientada a objetos',
+      description:
+        'Métodos y receptor. Constructores. Clases. Principios de diseño S.O.L.I.D. (3H)',
+    },
+    {
+      number: '04',
+      name: 'Programación funcional II',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones.(4H)',
+    },
+  ],
+  objetives: [
+    'Objetive 1',
+    'Objetive 2',
+    'Objetive 3',
+    'Objetive 4',
+    'Objetive N',
+  ],
+}
+
+const COURSE_REACT = {
+  id: 'react',
+  information: {
+    name: 'React',
+    nextCallDate: '2022-10-01',
+    description: "This is the best React course you'll ever find",
+    price: 1400,
+    hours: 25,
+    places: 30,
+    enrolledStudents: 28,
+    successPercentage: 98,
+  },
+  index: [
+    {
+      number: '01',
+      name: 'Fundamentos',
+      description:
+        'Introducción y recursos. String templates. Desestructuración. Declaración de variables. (2H)',
+    },
+    {
+      number: '02',
+      name: 'Programación funcional',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones. (2H)',
+    },
+    {
+      number: '03',
+      name: 'Programación orientada a objetos',
+      description:
+        'Métodos y receptor. Constructores. Clases. Principios de diseño S.O.L.I.D. (3H)',
+    },
+    {
+      number: '04',
+      name: 'Programación funcional II',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones.(4H)',
+    },
+  ],
+  objetives: [
+    'Objetive 1',
+    'Objetive 2',
+    'Objetive 3',
+    'Objetive 4',
+    'Objetive N',
+  ],
+}
+
+const COURSE_REDUX = {
+  id: 'redux',
+  information: {
+    name: 'Redux',
+    nextCallDate: '2022-09-01',
+    description: "This is the best Redux course you'll ever find",
+    price: 1300,
+    hours: 25,
+    places: 30,
+    enrolledStudents: 23,
+    successPercentage: 92,
+  },
+  index: [
+    {
+      number: '01',
+      name: 'Fundamentos',
+      description:
+        'Introducción y recursos. String templates. Desestructuración. Declaración de variables. (2H)',
+    },
+    {
+      number: '02',
+      name: 'Programación funcional',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones. (2H)',
+    },
+    {
+      number: '03',
+      name: 'Programación orientada a objetos',
+      description:
+        'Métodos y receptor. Constructores. Clases. Principios de diseño S.O.L.I.D. (3H)',
+    },
+    {
+      number: '04',
+      name: 'Programación funcional II',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones.(4H)',
+    },
+  ],
+  objetives: [
+    'Objetive 1',
+    'Objetive 2',
+    'Objetive 3',
+    'Objetive 4',
+    'Objetive N',
+  ],
+}
+
+const COURSE_DATA = {
+  id: 'data',
+  information: {
+    name: 'Data',
+    nextCallDate: '2022-08-01',
+    description: "This is the best Data course you'll ever find",
+    price: 1200,
+    hours: 30,
+    places: 20,
+    enrolledStudents: 9,
+    successPercentage: 94,
+  },
+  index: [
+    {
+      number: '01',
+      name: 'Fundamentos',
+      description:
+        'Introducción y recursos. String templates. Desestructuración. Declaración de variables. (2H)',
+    },
+    {
+      number: '02',
+      name: 'Programación funcional',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones. (2H)',
+    },
+    {
+      number: '03',
+      name: 'Programación orientada a objetos',
+      description:
+        'Métodos y receptor. Constructores. Clases. Principios de diseño S.O.L.I.D. (3H)',
+    },
+    {
+      number: '04',
+      name: 'Programación funcional II',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones.(4H)',
+    },
+  ],
+  objetives: [
+    'Objetive 1',
+    'Objetive 2',
+    'Objetive 3',
+    'Objetive 4',
+    'Objetive N',
+  ],
+}
+
+const COURSE_FULL_STACK = {
+  id: 'full-stack',
+  information: {
+    name: 'Full stack',
+    nextCallDate: '2022-07-01',
+    description: "This is the best Full stack course you'll ever find",
+    price: 1500,
+    hours: 50,
+    places: 30,
+    enrolledStudents: 18,
+    successPercentage: 93,
+  },
+  index: [
+    {
+      number: '01',
+      name: 'Fundamentos',
+      description:
+        'Introducción y recursos. String templates. Desestructuración. Declaración de variables. (2H)',
+    },
+    {
+      number: '02',
+      name: 'Programación funcional',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones. (2H)',
+    },
+    {
+      number: '03',
+      name: 'Programación orientada a objetos',
+      description:
+        'Métodos y receptor. Constructores. Clases. Principios de diseño S.O.L.I.D. (3H)',
+    },
+    {
+      number: '04',
+      name: 'Programación funcional II',
+      description:
+        'Higher order functions. Operaciones sobre listas. Composición de funciones.(4H)',
+    },
+  ],
+  objetives: [
+    'Objetive 1',
+    'Objetive 2',
+    'Objetive 3',
+    'Objetive 4',
+    'Objetive N',
+  ],
+}
+
+const COURSES = [
+  COURSE_JS_PRO,
+  COURSE_REACT,
+  COURSE_REDUX,
+  COURSE_DATA,
+  COURSE_FULL_STACK,
+]
+
+export default COURSES
