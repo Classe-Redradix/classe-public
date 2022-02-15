fac4ac0ee707b872ebeeea3b985b2ba4d7b7af58
rr21pepe <pepe@redradix.com>
2022-02-15 09:13:14 +0100

    ✨ Integrate `Course` component in `Course` page

--- src/app/course/Course.jsx
+++ src/app/course/Course.jsx
@@ -1,45 +1,37 @@
+import CourseUI from 'ui/views/course/Course'
+import COURSES from '../../../pages/data/courses'
+
+const courses = COURSES.map(course => ({
+  title: course.information.title,
+  href: `/courses/${course.id}`,
+}))
+
+const dates = [
+  {
+    day: '01',
+    month: 'Enero',
+    courses: [
+      {
+        title: 'Próxima convocatoria',
+        start: '16.01.21',
+        finish: '27.02.21',
+        to: '/',
+      },
+    ],
+  },
+]
+
 const Course = ({ course }) => {
-  const { information, index, objetives } = course
+  const { information } = course
 
   return (
-    <div
-      style={{
-        padding: '2rem',
-        maxWidth: '500px',
-        margin: 'auto',
-      }}
-    >
-      <h1>{information.title}</h1>
-      <p>{information.description}</p>
-      <p>Start: {information.start}</p>
-      <p>Finish: {information.finish}</p>
-      <p>{information.price}$</p>
-      <p>{information.hours} hours</p>
-      <p>{information.places} student places</p>
-      <p>{information.enrolledStudents} enrolled students</p>
-      <p>{information.successPercentage}% success</p>
-      <section>
-        <h2>Sections</h2>
-        <ol>
-          {index.map(section => (
-            <li key={`section-${section.number}`}>
-              <div>
-                <h3>{section.name}</h3>
-                <p>{section.description}</p>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </section>
-      <section>
-        <h2>Objetives</h2>
-        <ul>
-          {objetives.map((objetive, idx) => (
-            <li key={`objetive-${idx}`}>{objetive}</li>
-          ))}
-        </ul>
-      </section>
-    </div>
+    <CourseUI
+      isBlack
+      courses={courses}
+      dates={dates}
+      name={information.title}
+      title="objetivosd"
+    />
   )
 }
 
