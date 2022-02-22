1191740abe024b6f6c655a7b146c1f430aeb7619
rr21pepe <pepe@redradix.com>
2022-02-22 12:22:21 +0100

    💡 Disable `useEffect` in `MenuCourse`
    * This effect was causing an error when a course was closed

--- src/ui/components/menu/MenuCourse.jsx
+++ src/ui/components/menu/MenuCourse.jsx
@@ -28,25 +28,25 @@ const dates = [
 ]
 
 const MenuCourse = ({ course }) => {
-  gsap.registerPlugin(ScrollTrigger)
+  // gsap.registerPlugin(ScrollTrigger)
   const t = useTranslations()
   useScrambleText()
 
-  useEffect(() => {
-    const container = document.querySelectorAll('.courseSections')
-    gsap.to(container, {
-      x: () =>
-        -(container.scrollWidth - document.documentElement.clientWidth) + 'px',
-      ease: 'none',
-      scrollTrigger: {
-        trigger: container,
-        invalidateOnRefresh: true,
-        pin: true,
-        scrub: 1,
-        end: () => '+=' + container.offsetWidth,
-      },
-    })
-  }, [])
+  // useEffect(() => {
+  //   const container = document.querySelectorAll('.courseSections')
+  //   gsap.to(container, {
+  //     x: () =>
+  //       -(container.scrollWidth - document.documentElement.clientWidth) + 'px',
+  //     ease: 'none',
+  //     scrollTrigger: {
+  //       trigger: container,
+  //       invalidateOnRefresh: true,
+  //       pin: true,
+  //       scrub: 1,
+  //       end: () => '+=' + container.offsetWidth,
+  //     },
+  //   })
+  // }, [])
 
   const { information, index, objetives } = course
 
