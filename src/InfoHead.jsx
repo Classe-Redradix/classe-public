4583a42b26faeac5523e4db63e0c78183063effe
juanantonioledesma <juanantonioledesma@gmail.com>
2022-03-10 12:53:17 +0100

    Add more meta tags (viewport and, IE=edge)

--- src/InfoHead.jsx
+++ src/InfoHead.jsx
@@ -8,11 +8,13 @@ const InfoHead = ({ title, description, url, children }) => {
 
   return (
     <Head>
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
-
-      <meta name="robots" content="index, follow" />
 
       <link rel="canonical" href={url} />
 
