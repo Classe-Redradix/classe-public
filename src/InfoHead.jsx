d1e4d48be71dbfb6ed86791886820faa27538d20
juanantonioledesma <juanantonioledesma@gmail.com>
2022-03-10 12:24:08 +0100

    Add og images

--- src/InfoHead.jsx
+++ src/InfoHead.jsx
@@ -18,13 +18,16 @@ const InfoHead = ({ title, description, url, children }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={formatMessage('url:og-logo')} />
+      <meta property="og:image" content={formatMessage('url:og-1200x1200')} />
+      <meta property="og:image" content={formatMessage('url:og-921x518')} />
+      <meta property="og:image" content={formatMessage('url:og-400x400')} />
+      <meta property="og:image" content={formatMessage('url:og-256x256')} />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={formatMessage('url:og-logo')} />
+      <meta property="twitter:image" content={formatMessage('url:og-1200x1200')} />
 
       <link rel="manifest" href="manifest.json" />
 
