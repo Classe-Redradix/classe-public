4eacb357945769cd230e0ab634e8e0652202e1fa
Alejandro Mur <alejandro@redradix.com>
2022-03-17 11:47:47 +0100

    Fix(Storybook): add resolve-url-loadder to avoid errors when importing font paths from texts.scss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import { addDecorator, addParameters } from '@storybook/react'
 import * as NextImage from 'next/image'
 import withIntl from './addons/intl/withIntl'
 import { registerIcon } from '../src/styleguide/styleguide-config'
-import '!style-loader!css-loader!sass-loader!../src/assets/stylesheets/style.scss'
+import '!style-loader!css-loader!resolve-url-loader!sass-loader!../src/assets/stylesheets/style.scss'
 
 const OriginalNextImage = NextImage.default
 
