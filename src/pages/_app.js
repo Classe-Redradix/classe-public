ae5e82ee3bcab8f487afa22d7de3015d0fdf127e
juanantonioledesma <juanantonioledesma@gmail.com>
2022-03-22 14:41:10 +0100

    Comment sw.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,27 +4,27 @@ import './../assets/stylesheets/style.scss'
 import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker
-          .register('sw.js', {
-            scope: '.',
-          })
-          .then(
-            function (registration) {
-              console.log(
-                'Service Worker registration successful with scope: ',
-                registration.scope,
-              )
-            },
-            function (err) {
-              console.log('Service Worker registration failed: ', err)
-            },
-          )
-      })
-    }
-  }, [])
+  // useEffect(() => {
+  //   if ('serviceWorker' in navigator) {
+  //     window.addEventListener('load', function () {
+  //       navigator.serviceWorker
+  //         .register('sw.js', {
+  //           scope: '.',
+  //         })
+  //         .then(
+  //           function (registration) {
+  //             console.log(
+  //               'Service Worker registration successful with scope: ',
+  //               registration.scope,
+  //             )
+  //           },
+  //           function (err) {
+  //             console.log('Service Worker registration failed: ', err)
+  //           },
+  //         )
+  //     })
+  //   }
+  // }, [])
 
   return (
     <IntlAppProvider>
