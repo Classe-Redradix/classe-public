54f8e511b13ffd35e3c2e63587e51b6aeda3602a
juanantonioledesma <juanantonioledesma@gmail.com>
2022-07-06 18:45:08 +0200

    Use next Image

--- src/ui/components/sections/Find.jsx
+++ src/ui/components/sections/Find.jsx
@@ -1,17 +1,12 @@
+import Image from 'next/image'
 import PropTypes from 'prop-types'
 import { copyClasseEmailToClipboard } from '../../../business'
 import useTranslations from '../../../hooks/useTranslations'
 import SectionWrapper from '../wrappers/SectionWrapper'
 import Row from '../row/Row'
 import Cell from '../cell/Cell'
-import image480 from '/public/images/contacto-classe-480.jpg'
-import image935 from '/public/images/contacto-classe-935.jpg'
-import image1254 from '/public/images/contacto-classe-1254.jpg'
-import image1485 from '/public/images/contacto-classe-1485.jpg'
-import image1707 from '/public/images/contacto-classe-1707.jpg'
-import image1951 from '/public/images/contacto-classe-1951.jpg'
-import image2486 from '/public/images/contacto-classe-2486.jpg'
-import image3480 from '/public/images/contacto-classe-3480.jpg'
+
+import image from '/public/images/contacto-classe.jpg'
 
 import GlyphIcon from './../../../assets/icons/Glyph2Icon'
 
@@ -38,23 +33,16 @@ const Find = ({ isBlack, isFluor }) => {
           </div>
         </Cell>
         <Cell isNegative={isBlack}>
-          <picture>
-            <img
-              className="bwfilter"
-              sizes="(max-width: 3480px) 100vw, 3480px"
-              srcset={`${image480} 480w,
-              ${image935} 935w,
-              ${image1254} 1254w,
-              ${image1485} 1485w,
-              ${image1707} 1707w,
-              ${image1951} 1951w,
-              ${image2486} 2486w,
-              ${image3480} 3480w`}
-              src={image3480}
+          <div className="imageWrapper">
+            <Image
+              src={image}
               alt={t('alt-image:office-desks-computer-equipment')}
-              loading="lazy"
+              width={3480}
+              height={2320}
+              objectFit="cover"
+              layout="responsive"
             />
-          </picture>
+          </div>
         </Cell>
       </Row>
       <Row type="half">
