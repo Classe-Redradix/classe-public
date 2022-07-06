79badaf913528c1456e7f6ba85794e4e0dcf3dc1
juanantonioledesma <juanantonioledesma@gmail.com>
2022-07-06 13:35:15 +0200

    Optimize "contacto-classe" image

--- src/ui/components/sections/Find.jsx
+++ src/ui/components/sections/Find.jsx
@@ -4,7 +4,15 @@ import useTranslations from '../../../hooks/useTranslations'
 import SectionWrapper from '../wrappers/SectionWrapper'
 import Row from '../row/Row'
 import Cell from '../cell/Cell'
-import image from '/public/images/contacto-classe.jpg'
+import image480 from '/public/images/contacto-classe-480.jpg'
+import image935 from '/public/images/contacto-classe-935.jpg'
+import image1254 from '/public/images/contacto-classe-1254.jpg'
+import image1485 from '/public/images/contacto-classe-1485.jpg'
+import image1707 from '/public/images/contacto-classe-1707.jpg'
+import image1951 from '/public/images/contacto-classe-1951.jpg'
+import image2486 from '/public/images/contacto-classe-2486.jpg'
+import image3480 from '/public/images/contacto-classe-3480.jpg'
+
 import GlyphIcon from './../../../assets/icons/Glyph2Icon'
 
 const Find = ({ isBlack, isFluor }) => {
@@ -30,12 +38,23 @@ const Find = ({ isBlack, isFluor }) => {
           </div>
         </Cell>
         <Cell isNegative={isBlack}>
-          <img
-            className="bwfilter"
-            src={image}
-            alt={t('alt-image:office-desks-computer-equipment')}
-            loading="lazy"
-          />
+          <picture>
+            <img
+              className="bwfilter"
+              sizes="(max-width: 3480px) 100vw, 3480px"
+              srcset={`${image480} 480w,
+              ${image935} 935w,
+              ${image1254} 1254w,
+              ${image1485} 1485w,
+              ${image1707} 1707w,
+              ${image1951} 1951w,
+              ${image2486} 2486w,
+              ${image3480} 3480w`}
+              src={image3480}
+              alt={t('alt-image:office-desks-computer-equipment')}
+              loading="lazy"
+            />
+          </picture>
         </Cell>
       </Row>
       <Row type="half">
